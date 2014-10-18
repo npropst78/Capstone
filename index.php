07c8ae79cb35a271580e9a8b613143dd624642b9
npropst78 <npropst78@gmail.com>
2014-10-17 23:40:13 -0500

    Starter files

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * User: Nick
+ * Date: 10/17/2014
+ * Time: 11:33 PM
+ */
+
+php?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Login or Register</title>
+</head>
+
+<link href="assets/css/project.css" type="text/css" rel="stylesheet" />
+<body>
+
+</body>
+</html>
