7e4f6eccc73092919db8e10e89ebe9a620f50163
npropst78 <npropst78@gmail.com>
2014-11-14 03:06:23 -0600

    New Capstone additions

---
+++ EditPost.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nick
+ * Date: 11/14/2014
+ * Time: 3:05 AM
+ */ 
---
+++ Index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nick
+ * Date: 11/14/2014
+ * Time: 3:01 AM
+ */
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+</head>
+<link rel="stylesheet" href="assets/css/project.css" type="text/css"/>
+<link rel="stylesheet" href="assets/css/resetCss.css" type="text/css"/>
+
+<body>
+
+</body>
+</html>
